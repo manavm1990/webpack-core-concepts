18efede8616a5d09e11fb9fd7bc3cd31dc568660
Manav Misra <manavm@visionwebsoft.com>
2020-04-19 12:01:59 -0500

    :bulb: Add note 🎵about entire config file being optional for '4'
    :bulb: Add comment about optional 'output'

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
+// Since version 4.0.0, webpack does not require a configuration file to bundle your project.
+// https://webpack.js.org/concepts/
+
 module.exports = {
   // 'entry' should be 1 file
   entry: './src/index.js',
   mode: 'development',
+
+  // We could specify 'output', but default of 'src/main.js' is fine with me!
 };
